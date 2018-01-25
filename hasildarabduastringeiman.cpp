ebde7477257b8e38e4b8b98dd7d10e544b6453a6
Eiman Hakimy <33915454+eimanhakimy@users.noreply.github.com>
2018-01-25 10:21:26 +0800

    Add files via upload

---
+++ hasildarabduastringeiman.cpp
@@ -0,0 +1,33 @@
+#include <iostream> 
+using namespace std;
+
+class csifirdua
+{
+	int x;
+	int y;
+     public:
+	 void set_val(int,int);
+	 int hasilDarabdua()
+	 {
+	 	return x*y;
+	 } 	
+};
+void csifirdua::set_val(int a,int b)
+{
+	cout << "Sila Letakkan Nombor Yang Ingin Darab" << endl;
+
+	x=a;
+	y=b;
+	cout <<" Nombor Pertama : " << endl;
+	cin>>x;
+	cout << "Nombor Kedua :" << endl;
+	cin>>y;
+}
+
+ int main()
+
+{
+   csifirdua oA1;
+   oA1.set_val(5,6);
+   cout<<"Hasil Darab Dua   :"<<oA1.hasilDarabdua()<<endl;
+}
